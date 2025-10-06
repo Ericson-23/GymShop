36de1b2ffdca06b611a605195800cfe4e4d76352
Ericson-23 <ejedelapena.chmsu@gmail.com>
2025-10-06 23:01:03 +0800

    Add files via upload

---
+++ Gym_Guides/lib/auth.dart
@@ -0,0 +1,17 @@
+// auth.dart
+class UserAccount {
+  final String name;
+  final String username;
+  final String email;
+  final String password;
+
+  UserAccount({
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
+
+List<UserAccount> registeredUsers = [];
+UserAccount? currentUser;
